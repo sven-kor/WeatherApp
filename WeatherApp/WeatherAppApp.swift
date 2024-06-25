1dd2d7035d5e3e85fdfc08ba9d29aa8addc07112
Hyungjun Kim <hjhj919@naver.com>
2024-06-25 13:13:33 +0900

    start

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 김형준 on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
